02733a21d4e6876a3e080cd79c138ed0053358b7
李祖明 <solace.lee@outlook.com>
2022-07-13 11:18:10 +0800

    2D添加工具

--- src/page/app/cornerstone/imageRender.tsx
+++ src/page/app/cornerstone/imageRender.tsx
@@ -7,6 +7,8 @@ import { Button } from "antd";
 import { Request } from "@/common";
 import { initPVCore } from "../../../utils/cornerstonePV";
 import { useAppSelector } from '../../../store';
+import { ToolGroupManager, Enums as csToolsEnums } from "../../../utils/tools";
+import { ZoomTool, WindowLevelTool } from "../../../utils/tools/src";
 
 interface Props {
   keyValue: String
@@ -16,7 +18,7 @@ function ImageRender(props: Props) {
   const { keyValue } = props
   const pvImageIds = useAppSelector(state => state.pvImage.pvImageIds)
 
-  const renderingEngineRef: any = useRef(null)
+  const c3dRef: any = useRef({})
 
   async function renderImage(element: HTMLElement) {
 
@@ -26,7 +28,7 @@ function ImageRender(props: Props) {
     const viewportId = 'CT_AXIAL_STACK'
     const renderingEngine = new cornerstonePV.RenderingEngine(renderingEngineId)
 
-    renderingEngineRef.current = renderingEngine
+    c3dRef.current = { renderingEngine, viewportId, renderingEngineId }
 
     const viewportInput = {
       viewportId,
@@ -39,13 +41,18 @@ function ImageRender(props: Props) {
 
     let arr = pvImageIds?.map(v => {
       return 'wadouri:' + v
-    }) || []
-
-
-    // const result = await cornerstonePV.imageLoader.loadAndCacheImages(arr);
-    // Promise.all(result).then(res => {
-    //   console.log(res, 'result');
-    // });
+    }) || [
+        // 'http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.392.200036.9116.2.6.1.16.1613459813.1574739488.992936&seriesUID=1.2.392.200036.9116.2.6.1.16.1613459813.1574758542.892335&objectUID=1.2.392.200036.9116.2.6.1.16.1613459813.1574758750.204108&type=application%2Fdicom&collectionId=c7a5c4df-1041-4946-b5ed-4f6dc961d57c&patientId=325617a4-ad33-4479-a6e1-3e41c75ddad5&contentType=dcm-jpeg',
+        'wadouri:http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.840.113704.1.111.10216.1633940392.41&seriesUID=1.2.840.113704.1.111.10208.1633940850.8&objectUID=1.2.840.113704.1.111.8964.1633940884.5505&type=application%2Fdicom&collectionId=6e1a6eb5-e19d-4a1f-8a8d-48a50c15e822&patientId=3c2c1f6f-0031-4d5a-be98-4101d6f0371f&contentType=dcm-jpeg&index=87',
+        'wadouri:http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.840.113704.1.111.10216.1633940392.41&seriesUID=1.2.840.113704.1.111.10208.1633940850.8&objectUID=1.2.840.113704.1.111.8964.1633940884.5504&type=application%2Fdicom&collectionId=6e1a6eb5-e19d-4a1f-8a8d-48a50c15e822&patientId=3c2c1f6f-0031-4d5a-be98-4101d6f0371f&contentType=dcm-jpeg&index=88',
+        'wadouri:http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.840.113704.1.111.10216.1633940392.41&seriesUID=1.2.840.113704.1.111.10208.1633940850.8&objectUID=1.2.840.113704.1.111.8964.1633940884.5503&type=application%2Fdicom&collectionId=6e1a6eb5-e19d-4a1f-8a8d-48a50c15e822&patientId=3c2c1f6f-0031-4d5a-be98-4101d6f0371f&contentType=dcm-jpeg&index=89',
+        'wadouri:http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.840.113704.1.111.10216.1633940392.41&seriesUID=1.2.840.113704.1.111.10208.1633940850.8&objectUID=1.2.840.113704.1.111.8964.1633940884.5502&type=application%2Fdicom&collectionId=6e1a6eb5-e19d-4a1f-8a8d-48a50c15e822&patientId=3c2c1f6f-0031-4d5a-be98-4101d6f0371f&contentType=dcm-jpeg&index=90',
+        'wadouri:http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.840.113704.1.111.10216.1633940392.41&seriesUID=1.2.840.113704.1.111.10208.1633940850.8&objectUID=1.2.840.113704.1.111.8964.1633940884.5501&type=application%2Fdicom&collectionId=6e1a6eb5-e19d-4a1f-8a8d-48a50c15e822&patientId=3c2c1f6f-0031-4d5a-be98-4101d6f0371f&contentType=dcm-jpeg&index=91',
+        'wadouri:http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.840.113704.1.111.10216.1633940392.41&seriesUID=1.2.840.113704.1.111.10208.1633940850.8&objectUID=1.2.840.113704.1.111.8964.1633940884.5500&type=application%2Fdicom&collectionId=6e1a6eb5-e19d-4a1f-8a8d-48a50c15e822&patientId=3c2c1f6f-0031-4d5a-be98-4101d6f0371f&contentType=dcm-jpeg&index=92',
+        'wadouri:http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.840.113704.1.111.10216.1633940392.41&seriesUID=1.2.840.113704.1.111.10208.1633940850.8&objectUID=1.2.840.113704.1.111.8964.1633940884.5499&type=application%2Fdicom&collectionId=6e1a6eb5-e19d-4a1f-8a8d-48a50c15e822&patientId=3c2c1f6f-0031-4d5a-be98-4101d6f0371f&contentType=dcm-jpeg&index=93',
+        'wadouri:http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.840.113704.1.111.10216.1633940392.41&seriesUID=1.2.840.113704.1.111.10208.1633940850.8&objectUID=1.2.840.113704.1.111.8964.1633940884.5498&type=application%2Fdicom&collectionId=6e1a6eb5-e19d-4a1f-8a8d-48a50c15e822&patientId=3c2c1f6f-0031-4d5a-be98-4101d6f0371f&contentType=dcm-jpeg&index=94',
+        'wadouri:http://cleanown.cn:20009/api/main/wado?requestType=WADO&studyUID=1.2.840.113704.1.111.10216.1633940392.41&seriesUID=1.2.840.113704.1.111.10208.1633940850.8&objectUID=1.2.840.113704.1.111.8964.1633940883.5497&type=application%2Fdicom&collectionId=6e1a6eb5-e19d-4a1f-8a8d-48a50c15e822&patientId=3c2c1f6f-0031-4d5a-be98-4101d6f0371f&contentType=dcm-jpeg&index=95',
+      ]
 
     await (viewport as IStackViewport).setStack(arr, 3);
 
@@ -55,11 +62,49 @@ function ImageRender(props: Props) {
     (window as any).viewport = viewport;
 
     viewport.render()
+
+    addTools(viewportId, renderingEngineId) // 添加工具
+  }
+
+  // 添加工具
+  function addTools(viewportId: string, renderingEngineId: string) {
+    const toolGroupId = 'myToolGroup'
+    const toolGroup = ToolGroupManager.createToolGroup(toolGroupId)
+    c3dRef.current.toolGroupId = toolGroupId
+    console.log(toolGroup);
+    
+    toolGroup?.addTool(ZoomTool.toolName)
+    toolGroup?.addTool(WindowLevelTool.toolName)
+
+    toolGroup?.addViewport(viewportId, renderingEngineId)
   }
 
   function setNext(s: number) {
-    const viewport = renderingEngineRef.current.getViewport('CT_AXIAL_STACK')
+    const viewport = c3dRef.current.renderingEngine.getViewport('CT_AXIAL_STACK')
     viewport.scroll(s, true, true)
+  }
+
+  function zoomTool() {
+    const toolGroup = ToolGroupManager.getToolGroup(c3dRef.current.toolGroupId)
+    console.log(c3dRef.current, toolGroup);
+
+    toolGroup?.setToolActive(ZoomTool.toolName, {
+      bindings: [
+        {
+          mouseButton: csToolsEnums.MouseBindings.Secondary
+        }
+      ]
+    })
+  }
+
+  function wwc() {
+    c3dRef.current.toolGroup.setToolActive(WindowLevelTool.toolName, {
+      bindings: [
+        {
+          mouseButton: csToolsEnums.MouseBindings.Primary
+        }
+      ]
+    })
   }
 
 
@@ -82,6 +127,8 @@ function ImageRender(props: Props) {
   return <div>
     <Button onClick={() => setNext(-1)}>上一张</Button>
     <Button onClick={() => setNext(1)}>下一张</Button>
+    <Button onClick={zoomTool}>缩放工具</Button>
+    <Button onClick={wwc}>窗宽窗位工具</Button>
     <div id={`cornerstone3D-${keyValue}`}></div>
   </div>
 }
